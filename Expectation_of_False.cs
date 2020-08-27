6c8a507792f7596768570384bcdeaefb35bd1694
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-27 17:09:30 +0530

    Create Expectation_of_False.cs

---
+++ Expectation_of_False.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Diagnostics;
+class ExpectTrue
+{
+static void ExpectFalse(bool expression) {
+        if(expression) {
+            Console.WriteLine("Expected false, but got true");
+            Environment.Exit(1);
+        }
+    }
+ }   
