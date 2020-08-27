cac5d4f66590a1d26359119f05f1ab923ab93894
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-27 16:25:33 +0530

    Create Test.cs

---
+++ Test.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Diagnostics;
+class TestCases
+{
+ static int Main() {
+        ExpectTrue(vitalsAreOk(100, 95, 60));
+        ExpectFalse(vitalsAreOk(40, 91, 92));
+        Console.WriteLine("All ok");
+        return 0;
+    }
+}    
