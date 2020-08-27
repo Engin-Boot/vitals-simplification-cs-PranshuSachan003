833767c593832968612923333dcafbc67c2421bf
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-27 16:19:34 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -4,13 +4,11 @@ using System.Diagnostics;
 class Checker
 {
     static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-        if(bpm < 70 || bpm > 150) {
-            return false;
-        } else if(spo2 < 90) {
-            return false;
-        } else if(respRate < 30 || respRate > 95) {
-            return false;
-        }
+        var check_bpm = (bpm < 70 || bpm > 150);
+        var check_spo2 = (spo2 < 90);
+        var check_respRate = (respRate < 30 || respRate > 95);
+        if( check_bpm || check_spo2 || check_respRate)
+        return false;
         return true;
     }
     static void ExpectTrue(bool expression) {
