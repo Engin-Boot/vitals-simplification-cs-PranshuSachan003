9eaee1ff41e23c87d0b88af36734a284fc4b92bd
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-31 08:21:45 +0530

    Update Expectation_of_True.cs

--- Expectation_of_True.cs
+++ Expectation_of_True.cs
@@ -2,7 +2,6 @@ using System;
 using System.Diagnostics;
 class ExpectOfTrue
 {
-
  static void ExpectTrue(bool expression) {
         if(!expression) {
             Console.WriteLine("Expected true, but got false");
