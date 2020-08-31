37dfc3137856a0f8df924dde803e3da389815391
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-31 13:01:49 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -5,11 +5,11 @@ class Checker
     public static void Main()
     {
     static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-        var check_bpm = BPMisOk(bpm);
-        var check_spo2 = SPO2isOk(spo2);
-        var check_respRate = RESPRATEisOk(respRate);
-        if(check_bpm || check_spo2 || check_respRate)
-            return false;
+        var check_bpm = (bpm < 70 || bpm > 150);
+        var check_spo2 = (spo2 < 90);
+        var check_respRate = (respRate < 30 || respRate > 95);
+        if( check_bpm || check_spo2 || check_respRate)
+        return false;
         return true;
         }
      static void ExpectTrue(bool expression) {
