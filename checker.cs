2715969b85721247a63b8c952a8a9fac6a4e55b1
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-31 09:10:30 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -2,16 +2,6 @@ using System;
 using System.Diagnostics;
 class Checker
 {
-    public static void Main()
-    {
-    static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-        var check_bpm = (bpm < 70 || bpm > 150);
-        var check_spo2 = (spo2 < 90);
-        var check_respRate = (respRate < 30 || respRate > 95);
-        if( check_bpm || check_spo2 || check_respRate)
-        return false;
-        return true;
-    }
      static void ExpectTrue(bool expression) {
         if(!expression) {
             Console.WriteLine("Expected true, but got false");
