33875daa09d769615d16acd95a671037d5c9ea0f
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-31 09:18:36 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -14,7 +14,7 @@ class Checker
             Environment.Exit(1);
         }
     }
-    static int Main() {
+    public static void Main() {
         ExpectTrue(vitalsAreOk(100, 95, 60));
         ExpectFalse(vitalsAreOk(40, 91, 92));
         ExpectFalse(vitalsAreOk(50, 80, 20));
