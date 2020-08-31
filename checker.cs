ffe6ff98c35595312c0a8e43922f4cdc0fa617ad
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-31 09:54:51 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -24,23 +24,5 @@ class Checker
             Environment.Exit(1);
         }
     }
-    static int Main() {
-        ExpectTrue(vitalsAreOk(100, 95, 60));
-        ExpectFalse(vitalsAreOk(40, 91, 92));
-        ExpectFalse(vitalsAreOk(50, 80, 20));
-        ExpectFalse(vitalsAreOk(50, 80, 100));
-        ExpectFalse(vitalsAreOk(160, 80, 20));
-        ExpectFalse(vitalsAreOk(160, 80, 100));
-        ExpectFalse(vitalsAreOk(160, 91, 92));
-        ExpectFalse(vitalsAreOk(100, 80, 92));
-        ExpectFalse(vitalsAreOk(100, 91, 20));
-        ExpectFalse(vitalsAreOk(100, 91, 100));
-        ExpectFalse(vitalsAreOk(70, 90, 30));
-        ExpectFalse(vitalsAreOk(70, 90, 95));
-        ExpectFalse(vitalsAreOk(150, 90, 30));
-        ExpectFalse(vitalsAreOk(150, 90, 95));
-        Console.WriteLine("All ok");
-        return 0;
-    }
   }
 }
